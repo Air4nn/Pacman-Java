f695104b02ef50011d0ae4110dbe9eedccc8adf5
esteban300108-03 <esteban300108@gmail.com>
2026-06-09 18:15:15 -0500

    Add files via upload

---
+++ Pacman/src/main/java/com/mycompany/pacman/modelo/GestorPuntaje.java
@@ -0,0 +1,43 @@
+package com.mycompany.pacman.modelo;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GestorPuntaje {
+
+    private static final String ARCHIVO = "highscore.txt";
+
+    public int cargarHighScore() {
+
+        try {
+            BufferedReader lector = new BufferedReader(new FileReader(ARCHIVO));
+
+            int puntaje = Integer.parseInt(lector.readLine());
+
+            lector.close();
+
+            return puntaje;
+
+        } catch (Exception e) {
+
+            return 0;
+        }
+    }
+
+    public void guardarHighScore(int puntaje) {
+
+        try {
+            FileWriter escritor = new FileWriter(ARCHIVO);
+
+            escritor.write(String.valueOf(puntaje));
+
+            escritor.close();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
